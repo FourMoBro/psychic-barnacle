7bab83765d00dc9a2dc51e033a4c8d82751d0db8
fourmobro <lisyrj@gmail.com>
2016-11-15 13:59:26 +0000

    checked timestamp

--- matches/models.py
+++ matches/models.py
@@ -1,7 +1,12 @@
+import datetime
+
+
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+from .utils import get_match
 
 class MatchManager(models.Manager):
 	def get_or_create_match(self, user_a=None, user_b=None):
@@ -14,22 +19,21 @@ class MatchManager(models.Manager):
 		except:
 			obj_2 = None
 		if obj and not obj_2:
+			obj.check_update()
 			return obj, False
 		elif not obj and obj_2:
+			obj_2.check_update()
 			return obj_2, False
 		else:
 			new_instance = self.create(user_a=user_a, user_b=user_b)
-			#add match %
-			new_instance.match_decimal = 0.85
-			new_instance.questions_answered = 20
-			new_instance.save()
+			new_instance.do_match()
 			return new_instance, True
 			
 
 
 class Match(models.Model):
 	user_a = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_user_a')
-	user_b = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_user_b')
+	user_b 	= models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_user_b')
 	match_decimal = models.DecimalField(decimal_places=8, max_digits=16, default=0.00)
 	questions_answered = models.IntegerField(default=0)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
@@ -42,8 +46,22 @@ class Match(models.Model):
 
 	#good match?
 	#percentage value?
-	
 
+	def do_match(self):
+		user_a = self.user_a
+		user_b = self.user_b
+		match_decimal, questions_answered = get_match(user_a, user_b)
+		self.match_decimal = match_decimal
+		self.questions_answered = questions_answered
+		self.save()
+	
+	def check_update(self):
+		now = timezone.now()
+		offset = now - datetime.timedelta(hours=12)  # 12 hours ago
+		if self.updated <= offset:
+			self.do_match()
+		else:
+			print("already updated")
 
 
 
